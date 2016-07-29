8c749c608958ec3c349702a9f1f54878dd0321d3
aixiangfei <2792938834@qq.com>
2016-07-29 13:49:42 +0800

    update

--- init.sh
+++ init.sh
@@ -3,11 +3,11 @@
 cp -f .env.example .env
 php artisan key:generate
 
-sed -i "s/DB_CONNECTION=.*/DB_CONNECTION='$DB_TYPE'/g" .env
-sed -i "s/DB_HOST=.*/DB_HOST='$DB_HOST'/g" .env
-sed -i "s/DB_DATABASE=.*/DB_DATABASE='$DB_DATABASE'/g" .env
-sed -i "s/DB_USERNAME=.*/DB_USERNAME='$DB_USERNAME'/g" .env
-sed -i "s/DB_PASSWORD=.*/DB_PASSWORD='$DB_PASSWORD'/g" .env
+sed -i 's/DB_CONNECTION=.*/DB_CONNECTION=$DB_TYPE/g' .env
+sed -i 's/DB_HOST=.*/DB_HOST=$DB_HOST/g' .env
+sed -i 's/DB_DATABASE=.*/DB_DATABASE=$DB_DATABASE/g' .env
+sed -i 's/DB_USERNAME=.*/DB_USERNAME=$DB_USERNAME/g' .env
+sed -i 's/DB_PASSWORD=.*/DB_PASSWORD=$DB_PASSWORD/g' .env
 
 php artisan migrate
 
