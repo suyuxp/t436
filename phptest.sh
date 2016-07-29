3cc8bb24303deda2648a40561b4af41e97836d7d
aixiangfei <2792938834@qq.com>
2016-07-29 14:00:58 +0800

    update

--- phptest.sh
+++ phptest.sh
@@ -3,7 +3,7 @@
 cp -f .env.example .env
 php artisan key:generate
 
-sed -i 's/DB_CONNECTION=.*/DB_CONNECTION=$DB_TYPE/g' .env
+sed -i 's/DB_CONNECTION=.*/DB_CONNECTION=$DB_CONNECTION/g' .env
 sed -i 's/DB_HOST=.*/DB_HOST=$DB_HOST/g' .env
 sed -i 's/DB_DATABASE=.*/DB_DATABASE=$DB_DATABASE/g' .env
 sed -i 's/DB_USERNAME=.*/DB_USERNAME=$DB_USERNAME/g' .env
